91b58102e6c15e247f53a595426b8d433f685d99
r4web <contact@r4web.fr>
2025-05-26 13:34:49 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,9 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://front-aeropaperasse.vercel.app'
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Authorization'],
